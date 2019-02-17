d44c4867197c6d1a9310f0c9158eab7a9f6a94c8
Zach Shaw <zach@Zachs-MacBook-Pro.local>
2019-02-17 16:34:41 +0000

    adding places query and removing node modules

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,20 @@
-const Query = {};
+const axios = require('axios');
+
+const GOOGLE_PLACES_API_KEY = process.env.GOOGLE_PLACES_API_KEY;
+
+const fetchGooglePlace = (id) => {
+  return `https://maps.googleapis.com/maps/api/place/details/json?placeid=${id}&key=${GOOGLE_PLACES_API_KEY}`
+};
+
+const Query = {
+  async googlePlaceWithId(parent, args, ctx, info) {
+    const { id } = args;
+    const response = await axios.get(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${id}&key=${GOOGLE_PLACES_API_KEY}`);
+    const payload = JSON.stringify(response.data.result);
+    return {
+      payload
+    };
+  }
+};
 
 module.exports = Query;
